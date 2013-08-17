fddf2ce040a7c561e2a3a9b5ea8620cd3f13743a
lasitha1991 <lasitha1991@hotmail.com>
2013-08-17 14:31:09 +0530

    first commit from lap

---
+++ mainwindow.cpp
@@ -0,0 +1,44 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <vlc/vlc.h>
+#include <player.h>
+
+bool isplay=true;
+player *p=new player();
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QObject::connect(ui->Btnplay,SIGNAL(clicked()),this,SLOT(on_Btnplay_clicked()));
+
+
+    ui->display->setAutoFillBackground( true );
+    QPalette plt = palette();
+    plt.setColor( QPalette::Window, Qt::black );
+    ui->display->setPalette( plt );
+
+    p->load(ui->display);
+
+    ui->Btnplay->setText("play");
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_Btnplay_clicked()
+{
+    p->play(ui->Btnplay);
+}
+
+void MainWindow::on_BtnStream_clicked()
+{
+    p->stream(ui->BtnStream);
+}
